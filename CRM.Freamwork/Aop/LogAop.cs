86bc72b553574944b6ba20b255e48abe4048d521
XuRuiO <xhxy@live.cn>
2019-06-03 19:07:00 +0800

    签入

---
+++ CRM.Freamwork/Aop/LogAop.cs
@@ -0,0 +1,55 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CRM.Freamwork.Aop
+{
+    /// <summary>
+    /// 2019.05.30      Rui     日志记录Aop拦截器，继承接口IInterceptor
+    /// </summary>
+    public class LogAop : IInterceptor
+    {
+        /// <summary>
+        /// 实例化接口IINterceptor的唯一方法Intercept
+        /// </summary>
+        /// <param name="invocation"></param>
+        public void Intercept(IInvocation invocation)
+        {            //记录被拦截方法信息的日志信息
+            var dataIntercept = $@"{DateTime.Now.ToString("yyyyMMddHHmmss")}/r/n
+                                    当前执行方法：{invocation.Method.Name}/r/n
+                                    参数是：{string.Join(",", invocation.Arguments.Select(x => (x ?? "").ToString()).ToArray())}";
+
+            try
+            {
+                //在被拦截的方法执行完毕之后，继续执行当前方法
+                invocation.Proceed();
+            }
+            catch (Exception ex)
+            {
+                //执行的service 中，出现异常
+                dataIntercept += ($"方法执行中，出现异常：{ex.Message + ex.InnerException}");
+            }
+
+            dataIntercept += ($"被拦截的方法执行完毕，返回结果：{invocation.ReturnValue}");
+
+            #region 输出到当前项目日志
+
+            var path = Directory.GetCurrentDirectory() + @"\Log";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string fileName = path + $@"\InterceptLog-{DateTime.Now.ToString("yyyyMMddHHmmss")}.log";
+
+            StreamWriter sw = File.AppendText(fileName);
+            sw.WriteLine(dataIntercept);
+            sw.Close();
+
+            #endregion
+        }
+    }
+}
--- CRM.Freamwork/Autofac/AutofacExtension.cs
+++ CRM.Freamwork/Autofac/AutofacExtension.cs
@@ -1,6 +1,8 @@
 ﻿using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Autofac.Extras.DynamicProxy;
 using CRM.Core.Helpers;
+using CRM.Freamwork.Aop;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -24,6 +26,13 @@ namespace CRM.Freamwork.Autofac
         {
             ////第一步：构造一个AutoFac的builder容器。
             var builder = new ContainerBuilder();
+
+            #region 注册拦截器Aop（由于Aop还不太理解，暂时项目中没用起来Aop这一块，还是要多看博客园之 《老张的哲学》）
+
+            //日志拦截器
+            //builder.RegisterType<LogAop>();
+
+            #endregion
 
             //第二步：告诉AutoFac容器，创建项目中的指定类的对象实例，以接口的形式存储（其实就是创建数据仓储层与业务逻辑层这两个程序集中所有类的对象实例，然后以其接口的形式保存到AutoFac容器内存中，当然如果有需要也可以创建其他程序集的所有类的对象实例，这个只需要我们指定就可以了）
             //获取项目路径
