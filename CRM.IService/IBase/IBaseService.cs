0aa001c523af90dbf9c71e51043f79384122795d
XuRuiO <xhxy@live.cn>
2019-07-31 18:42:54 +0800

    新增多表分页查询

--- CRM.IService/IBase/IBaseService.cs
+++ CRM.IService/IBase/IBaseService.cs
@@ -180,6 +180,96 @@ namespace CRM.IService.IBase
 
         /// <summary>
         /// 多表查询
+        /// 根据自定义的表达式，返回匿名对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="TResult">返回匿名对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchAnonymityAsync<T1, T2, T3, T4, TResult>(Expression<Func<T1, T2, T3, T4, JoinQueryInfos>> joinExpression, Expression<Func<T1, T2, T3, T4, TResult>> selectExpression, Expression<Func<T1, T2, T3, T4, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
+        /// 根据自定义的表达式，返回匿名对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="TResult">返回匿名对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchAnonymityAsync<T1, T2, T3, T4, T5, TResult>(Expression<Func<T1, T2, T3, T4, T5, JoinQueryInfos>> joinExpression, Expression<Func<T1, T2, T3, T4, T5, TResult>> selectExpression, Expression<Func<T1, T2, T3, T4, T5, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
+        /// 根据自定义的表达式，返回匿名对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="T6">实体6</typeparam>
+        /// <typeparam name="TResult">返回匿名对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchAnonymityAsync<T1, T2, T3, T4, T5, T6, TResult>(Expression<Func<T1, T2, T3, T4, T5, T6, JoinQueryInfos>> joinExpression, Expression<Func<T1, T2, T3, T4, T5, T6, TResult>> selectExpression, Expression<Func<T1, T2, T3, T4, T5, T6, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
+        /// 根据自定义的表达式，返回匿名对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="T6">实体6</typeparam>
+        /// <typeparam name="T7">实体7</typeparam>
+        /// <typeparam name="TResult">返回匿名对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchAnonymityAsync<T1, T2, T3, T4, T5, T6, T7, TResult>(Expression<Func<T1, T2, T3, T4, T5, T6, T7, JoinQueryInfos>> joinExpression, Expression<Func<T1, T2, T3, T4, T5, T6, T7, TResult>> selectExpression, Expression<Func<T1, T2, T3, T4, T5, T6, T7, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
+        /// 根据自定义的表达式，返回匿名对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="T6">实体6</typeparam>
+        /// <typeparam name="T7">实体7</typeparam>
+        /// <typeparam name="T8">实体8</typeparam>
+        /// <typeparam name="TResult">返回匿名对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchAnonymityAsync<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, JoinQueryInfos>> joinExpression, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult>> selectExpression, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
         /// 根据自定义的实体对象，返回实体对象集合数据
         /// </summary>
         /// <typeparam name="T1">实体1</typeparam>
@@ -188,8 +278,244 @@ namespace CRM.IService.IBase
         /// <param name="joinExpression">关联表达式 (t1,t2) => new JoinQueryInfos(JoinType.Inner, t1.UserNo==t2.UserNo)</param>
         /// <param name="selectExpression">自定义指定实体对象（支持自动填充），返回实体对象集合数据</param>
         /// <param name="whereExpression">条件表达式 (t1, t2) =>t1.UserNo == "")</param>
+        /// <param name="orderMuch">排序条件</param>
         /// <returns></returns>
-        Task<List<TResult>> QueryMuchEntityAsync<T1, T2, TResult>(Expression<Func<T1, T2, JoinQueryInfos>> joinExpression, TResult selectExpression, Expression<Func<T1, T2, bool>> whereExpression = null) where T1 : class, new();
+        Task<List<TResult>> QueryMuchEntityAsync<T1, T2, TResult>(Expression<Func<T1, T2, JoinQueryInfos>> joinExpression, TResult selectExpression, Expression<Func<T1, T2, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
+        /// 根据自定义的实体对象，返回实体对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="TResult">返回实体对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchEntityAsync<T1, T2, T3, TResult>(Expression<Func<T1, T2, T3, JoinQueryInfos>> joinExpression, TResult selectExpression, Expression<Func<T1, T2, T3, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
+        /// 根据自定义的实体对象，返回实体对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="TResult">返回实体对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchEntityAsync<T1, T2, T3, T4, TResult>(Expression<Func<T1, T2, T3, T4, JoinQueryInfos>> joinExpression, TResult selectExpression, Expression<Func<T1, T2, T3, T4, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
+        /// 根据自定义的实体对象，返回实体对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="TResult">返回实体对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchEntityAsync<T1, T2, T3, T4, T5, TResult>(Expression<Func<T1, T2, T3, T4, T5, JoinQueryInfos>> joinExpression, TResult selectExpression, Expression<Func<T1, T2, T3, T4, T5, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
+        /// 根据自定义的实体对象，返回实体对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="T6">实体6</typeparam>
+        /// <typeparam name="TResult">返回实体对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchEntityAsync<T1, T2, T3, T4, T5, T6, TResult>(Expression<Func<T1, T2, T3, T4, T5, T6, JoinQueryInfos>> joinExpression, TResult selectExpression, Expression<Func<T1, T2, T3, T4, T5, T6, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
+        /// 根据自定义的实体对象，返回实体对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="T6">实体6</typeparam>
+        /// <typeparam name="T7">实体7</typeparam>
+        /// <typeparam name="TResult">返回实体对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchEntityAsync<T1, T2, T3, T4, T5, T6, T7, TResult>(Expression<Func<T1, T2, T3, T4, T5, T6, T7, JoinQueryInfos>> joinExpression, TResult selectExpression, Expression<Func<T1, T2, T3, T4, T5, T6, T7, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表查询
+        /// 根据自定义的实体对象，返回实体对象集合数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="T6">实体6</typeparam>
+        /// <typeparam name="T7">实体7</typeparam>
+        /// <typeparam name="T8">实体8</typeparam>
+        /// <typeparam name="TResult">返回实体对象</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryMuchEntityAsync<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, JoinQueryInfos>> joinExpression, TResult selectExpression, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        #endregion
+
+        #region 多表分页查询操作
+
+        /// <summary>
+        /// 多表分页查询
+        /// 根据自定义的实体对象，返回实体对象集合分页数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="TResult">返回实体对象集合分页数据</typeparam>
+        /// <param name="joinExpression">关联表达式 (t1,t2) => new JoinQueryInfos(JoinType.Inner, t1.UserNo==t2.UserNo)</param>
+        /// <param name="selectExpression">自定义指定实体对象（支持自动填充），返回实体对象集合数据</param>
+        /// <param name="pageInfo">分页信息</param>
+        /// <param name="whereExpression">条件表达式 (t1, t2) =>t1.UserNo == "")</param>
+        /// <param name="orderMuch">排序条件</param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryPageMuchEntityAsync<T1, T2, TResult>(Expression<Func<T1, T2, JoinQueryInfos>> joinExpression, TResult selectExpression, SqlSugarPageInfo pageInfo, Expression<Func<T1, T2, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表分页查询
+        /// 根据自定义的实体对象，返回实体对象集合分页数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="TResult">返回实体对象集合分页数据</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="pageInfo"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryPageMuchEntityAsync<T1, T2, T3, TResult>(Expression<Func<T1, T2, T3, JoinQueryInfos>> joinExpression, TResult selectExpression, SqlSugarPageInfo pageInfo, Expression<Func<T1, T2, T3, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表分页查询
+        /// 根据自定义的实体对象，返回实体对象集合分页数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="TResult">返回实体对象集合分页数据</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="pageInfo"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryPageMuchEntityAsync<T1, T2, T3, T4, TResult>(Expression<Func<T1, T2, T3, T4, JoinQueryInfos>> joinExpression, TResult selectExpression, SqlSugarPageInfo pageInfo, Expression<Func<T1, T2, T3, T4, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表分页查询
+        /// 根据自定义的实体对象，返回实体对象集合分页数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="TResult">返回实体对象集合分页数据</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="pageInfo"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryPageMuchEntityAsync<T1, T2, T3, T4, T5, TResult>(Expression<Func<T1, T2, T3, T4, T5, JoinQueryInfos>> joinExpression, TResult selectExpression, SqlSugarPageInfo pageInfo, Expression<Func<T1, T2, T3, T4, T5, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表分页查询
+        /// 根据自定义的实体对象，返回实体对象集合分页数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="T6">实体6</typeparam>
+        /// <typeparam name="TResult">返回实体对象集合分页数据</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="pageInfo"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryPageMuchEntityAsync<T1, T2, T3, T4, T5, T6, TResult>(Expression<Func<T1, T2, T3, T4, T5, T6, JoinQueryInfos>> joinExpression, TResult selectExpression, SqlSugarPageInfo pageInfo, Expression<Func<T1, T2, T3, T4, T5, T6, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表分页查询
+        /// 根据自定义的实体对象，返回实体对象集合分页数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="T6">实体6</typeparam>
+        /// <typeparam name="T7">实体7</typeparam>
+        /// <typeparam name="TResult">返回实体对象集合分页数据</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="pageInfo"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryPageMuchEntityAsync<T1, T2, T3, T4, T5, T6, T7, TResult>(Expression<Func<T1, T2, T3, T4, T5, T6, T7, JoinQueryInfos>> joinExpression, TResult selectExpression, SqlSugarPageInfo pageInfo, Expression<Func<T1, T2, T3, T4, T5, T6, T7, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
+
+        /// <summary>
+        /// 多表分页查询
+        /// 根据自定义的实体对象，返回实体对象集合分页数据
+        /// </summary>
+        /// <typeparam name="T1">实体1</typeparam>
+        /// <typeparam name="T2">实体2</typeparam>
+        /// <typeparam name="T3">实体3</typeparam>
+        /// <typeparam name="T4">实体4</typeparam>
+        /// <typeparam name="T5">实体5</typeparam>
+        /// <typeparam name="T6">实体6</typeparam>
+        /// <typeparam name="T7">实体7</typeparam>
+        /// <typeparam name="T8">实体8</typeparam>
+        /// <typeparam name="TResult">返回实体对象集合分页数据</typeparam>
+        /// <param name="joinExpression"></param>
+        /// <param name="selectExpression"></param>
+        /// <param name="pageInfo"></param>
+        /// <param name="whereExpression"></param>
+        /// <param name="orderMuch"></param>
+        /// <returns></returns>
+        Task<List<TResult>> QueryPageMuchEntityAsync<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, JoinQueryInfos>> joinExpression, TResult selectExpression, SqlSugarPageInfo pageInfo, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, bool>> whereExpression = null, List<OrderByClause> orderMuch = null) where T1 : class, new();
 
         #endregion
     }
